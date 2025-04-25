0389ab6823906760e3922a344dd2297c4e26ca94
Christian S. Perone <christian.perone@gmail.com>
2025-04-25 16:15:26 +0000

    Fixing debugging messages.

--- core/vision_encoder/pe.py
+++ core/vision_encoder/pe.py
@@ -634,8 +634,6 @@ class TextTransformer(nn.Module):
         
         logger.info(f"Missing keys for loading model: {m}")
         logger.info(f"Unexpected keys for loading model: {u}")
-        print(f"Missing keys for loading model: {m}")
-        print(f"Unexpected keys for loading model: {u}")
 
     def build_cls_mask(self, text):
         cls_mask = (text != self.pad_id).unsqueeze(1)
